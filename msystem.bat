8215e8d7376376e9af05b972556ac6702c45226a
Stefan Zimmermann <zimmermann.code@gmail.com>
2016-02-12 18:50:39 +0100

    added /W and /-W flags to msystem.bat and improved arg parsing and %PATH% handling

--- msystem.bat
+++ msystem.bat
@@ -5,21 +5,32 @@ REM Copyright (C) 2015-2016 Stefan Zimmermann <zimmermann.code@gmail.com>
 REM
 REM Licensed under the Apache License, Version 2.0
 
+setlocal EnableExtensions || exit /b
 
 if "%~1" == "/?" (
     echo Activates an MSYS2 environment inside a CMD shell,
     echo either in MSYS or MINGW32 or MINGW64 mode,
-    echo by prepending the relevant MSYS2 bin\ paths to %%PATH%%.
+    echo by prepending the according MSYS2 bin\ paths to %%PATH%%.
     echo.
-    echo MSYSTEM [MSYS ^| MINGW32 ^| MINGW64]
+    echo MSYSTEM [/S] [MSYS ^| MINGW32 ^| MINGW64] [/W ^| /-W]]
     echo.
-    echo The MINGW modes do NOT include the MSYS mode bin\ paths.
+    echo   /S   Combine MINGW32 or MINGW64 with MSYS mode and its bin\ paths.
+    echo   /W   Force opening a new CMD window.
+    echo   /-W  Explicitly avoid opening a new CMD window.
     echo.
-    echo In MSYS mode, an additional path containing wrapper scripts
-    echo for BASH and PACMAN is prepended to %%PATH%%.
+    echo In MSYS mode, MSystem's msys\ sub-directory with wrapper scripts
+    echo BASH, PACMAN, MAKEPKG, and MAKEPKG-MINGW
+    echo is also prepended to %%PATH%%.
+    echo Call any of those scripts with /? for details after activation.
     echo.
-    echo To work properly, either this script must be located
-    echo in a cmd\ sub-directory of the MSYS2 installation,
+    echo In MINGW modes, MSystem's mingw32\ or mingw64\ path
+    echo with MINGW mode BASH wrapper scripts is prepended to %%PATH%%.
+    echo Call BASH /? for details after activation.
+    echo With given /S, the additional wrapper scripts of MSYS mode
+    echo will also be available.
+    echo.
+    echo To work properly, either MSystem must be installed
+    echo as a direct sub-directory of the MSYS2 installation,
     echo or the root directory of the MSYS2 installation
     echo ^(usually C:\path\to\msys32 or ...\msys64^)
     echo must be defined via %%MSYS2_ROOT%% or must be in %%PATH%%.
@@ -27,8 +38,15 @@ if "%~1" == "/?" (
     echo After successful activation,
     echo the environment name will be stored in %%MSYS2_SYSTEM%%
     echo and an according ^<MSYS^> or ^<MINGW32^> or ^<MINGW64^> tag
+    echo ^(or ^<MINGW32^|MSYS^> or ^<MINGW64^|MSYS^> if activated with /S^)
     echo will be prepended to %%PROMPT%%.
     echo.
+    echo If MSYSTEM is called from a non-interactive CMD instance
+    echo ^(from another shell or graphical interface^),
+    echo MSYSTEM will automatically start a new CMD window.
+    echo You can also force a new window with /W
+    echo and explicitly avoid a new window with /-W.
+    echo
     echo Call MSYSTEM without an argument
     echo to show the currently active MSYS2 environment.
     echo.
@@ -72,18 +90,22 @@ REM Handle install flag
 if /i "%~1" == "/X" (
     REM install MSYS2 features into other CMD shell extensions
     shift /1
+    endlocal
     goto :install
 )
 if /i "%~1" == "/I" (
     echo The /I flag is deprecated. Please use MSYSTEM /X in the future.
     shift /1
+    endlocal
     goto :install
 )
+
+endlocal
 
 REM ==========================================================================
 REM Environment management
 
-setlocal EnableDelayedExpansion
+setlocal EnableExtensions EnableDelayedExpansion || exit /b
 
 REM --------------------------------------------------------------------------
 REM Handle environment deactivation flag
@@ -94,30 +116,68 @@ if /i "%~1" == "/D" (
 )
 
 REM --------------------------------------------------------------------------
-REM Handle activation flags in a loop
+REM Parse arguments in a loop
 
 REM default options
+set newWindow=auto
 set MSYS=false
 
-:parseFlags
+:parseArgs
+
+if /i "%~1" == "/W" (
+    set newWindow=true
+    shift /1
+    goto :parseArgs
+)
+
+if /i "%~1" == "/-W" (
+    set newWindow=false
+    shift /1
+    goto :parseArgs
+)
 
 if /i "%~1" == "/S" (
     set MSYS=true
     shift /1
-    goto :parseFlags
+    goto :parseArgs
 )
 
-REM --------------------------------------------------------------------------
 REM Check for valid MSYS2 environment name and set %MSYS2_SYSTEM% accordingly
-
 for %%M in (MSYS MINGW32 MINGW64) do if /i "%~1" == "%%M" (
     set MSYS2_SYSTEM=%%M
     shift /1
-    goto :prepare
+    goto :parseArgs
 )
-echo Invalid argument '%~1'.
-echo Call MSYSTEM /? for help.
-exit /b 1
+
+for %%F in (/X /I) do if /i "%~1" == "%%F" (
+    echo MSYSTEM\\ERROR: %%F can only be used as first argument.
+    echo Call MSYSTEM /? for help.
+    exit /b 1
+)
+
+if not "%~1" == "" (
+    echo MSYSTEM\\ERROR: Invalid argument '%~1'.
+    echo Call MSYSTEM /? for help.
+    exit /b 1
+)
+
+REM --------------------------------------------------------------------------
+REM Handle request or need for new CMD window
+:window
+
+if %newWindow% == auto (
+    REM check if started in non-interactive CMD process (via cmd /c ...)
+    set "_cmdcmdline=!cmdcmdline!"
+    if /i not "!_cmdcmdline: /c =!" == "!cmdcmdline!" (
+        echo MSYSTEM: Detected non-interactive CMD shell!
+        set newWindow=true
+    )
+)
+if %newWindow% == true (
+    echo MSYSTEM: Starting MSystem in new CMD shell window...
+    start cmd /k cmd /k "%~f0" %* /-w
+    exit /b
+)
 
 REM --------------------------------------------------------------------------
 REM Check system and create variables for MSYS2 (de)activation
@@ -152,12 +212,13 @@ if not exist "%MSYS2_ROOT%\msys2_shell.bat" (
     echo Should be in ^<MSYS2 root^>\cmd\
     exit /b 1
 )
-goto deactivate
+goto :deactivate
 
 :customRoot
 
 if not exist "%MSYS2_ROOT%\" (
-    echo %%MSYS2_ROOT%% '%MSYS2_ROOT%' does not exist or is not a directory.
+    set "error=does not exist or is not a directory"
+    echo MSYSTEM\\ERROR: %%MSYS2_ROOT%% '%MSYS2_ROOT%' !error!.
     exit /b 1
 )
 
@@ -175,8 +236,10 @@ if not "%MSYS2_MINGW32_PATH%" == "" (
 if not "%MSYS2_MINGW64_PATH%" == "" (
     call set "PATH=%%PATH:%MSYS2_MINGW64_PATH%;=%%"
 )
-REM and also remove the extra MSYS tool wrapper scripts path
+REM and also remove any extra MSYS/MINGW tool wrapper scripts paths
 call set "PATH=%%PATH:%~dp0msys;=%%"
+call set "PATH=%%PATH:%~dp0mingw32;=%%"
+call set "PATH=%%PATH:%~dp0mingw64;=%%"
 
 REM remove any <MSYS> or <MINGW32/64> tags from %PROMPT%
 REM (use temporary %_cleanPrompt% to avoid too many %PROMPT% changes,
@@ -195,7 +258,7 @@ if "%MSYS2_SYSTEM%" == "" (
     set MSYS2_MINGW32_PATH=
     set MSYS2_MINGW64_PATH=
 
-    goto end
+    goto :end
 )
 
 REM --------------------------------------------------------------------------
